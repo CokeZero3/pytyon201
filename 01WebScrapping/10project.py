1cedae66fc12f3eb746de3dfc30fdaefea418e19
wonho.jeong <jeongwh0310@gmail.com>
2021-01-01 23:46:08 +0900

    update

--- 01WebScrapping/10project.py
+++ 01WebScrapping/10project.py
@@ -17,13 +17,18 @@ from datetime import datetime
 
 
 #나도코딩 방법
+def create_soup(url):
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
+    return soup
+    
 def scrape_weather():
     now = datetime.now()
     print("[{}년 {}월 {}일, 오늘의 날씨]".format(now.year, now.month, now.day))
     url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%84%9C%EC%9A%B8+%EB%82%A0%EC%94%A8" 
-    res = requests.get(url)
-    res.raise_for_status()
-    soup = BeautifulSoup(res.text, "lxml")
+    soup = create_soup(url)
+  
     
     # temp = soup.find("span", attrs={"class":"todaytemp"}).get_text() #
     curr_temp = soup.find("p", attrs={"class":"info_temperature"}).get_text().replace("도씨", "")
@@ -31,8 +36,9 @@ def scrape_weather():
     min_temp = soup.find("span", attrs={"class":"min"}).get_text() #최고 온도
     weather = soup.find("p", attrs={"class":"cast_txt"}).get_text()
 
-    am_rain_rate = soup.find("span", attrs={"class":"point_time morning"}).find("span", attrs={"class":"rain_rate wet"}).get_text()
-    pm_rain_rate = soup.find("span", attrs={"class":"point_time afternoon"}).find("span", attrs={"class":"rain_rate"}).get_text()
+    #오전 강수 확률, 오후 강수확률
+    am_rain_rate = soup.find("span", attrs={"class":"point_time morning"}).get_text().strip()
+    pm_rain_rate = soup.find("span", attrs={"class":"point_time afternoon"}).get_text().strip()
     
     # dust = soup.find("dl", attrs={"class":"indicator"}, text=["미세먼지", "초미세먼지"]).get_text() # 미세먼지
     dust = soup.find("dl", attrs={"class":"indicator"}) # 미세먼지
@@ -49,39 +55,21 @@ def scrape_weather():
     print("초미세먼지 {}".format(pm25))
     print("오존지수 {}".format(ozone))
 
+def scrape_headline_news():
+    print("[헤드라인 뉴스]")
+    url ="https://news.naver.com"
+    soup = create_soup(url)
+    news_list = soup.find("ul", attrs={"class":"hdline_article_list"}).find_all("li")
+    
+    for index, news in enumerate(news_list):
+        title = news.find("a").get_text().strip()
+        link = url + news.find("a")["href"]
+        print("{}. {}".fomrat(index+1, title))
+        print(" (링크: {})".format(link))
+    print()
+    
+    
+                
 if __name__ == "__main__":
-    scrape_weather() #오늘의 날씨 정보 가져오기
-
-
-"""#Won's Answer
-#날짜 구하기
-now = datetime.now()
-# print(now.year, now.month, now.day)
-
-url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%84%9C%EC%9A%B8+%EB%82%A0%EC%94%A8" 
-res = requests.get(url)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")
-
-#1. 오늘의 날씨 구하기
-temp = soup.find("span", attrs={"class":"todaytemp"}).get_text()
-max_temp = soup.find("span", attrs={"class":"max"}).get_text()
-min_temp = soup.find("span", attrs={"class":"min"}).get_text()
-weather = soup.find("p", attrs={"class":"cast_txt"}).get_text()
-am_rain_rate = soup.find("span", attrs={"class":"point_time morning"}).find("span", attrs={"class":"rain_rate wet"}).get_text()
-pm_rain_rate = soup.find("span", attrs={"class":"point_time afternoon"}).find("span", attrs={"class":"rain_rate"}).get_text()
-lv1 = soup.find("dd", attrs={"class":"lv1"}).get_text() # 오존지수
-lv2 = soup.find("dd", attrs={"class":"lv2"}).get_text() # 미세먼지
-lv3 = soup.find("dd", attrs={"class":"lv3"}).get_text() # 초미세먼지
-
-print("[{}년 {}월 {}일, 오늘의 날씨]".format(now.year, now.month, now.day))
-print(weather)
-print("현재 {}˚C (최저 {}/ 최고 {})".format(temp, min_temp, max_temp))
-print("오전 {} / 오후 {}".format(am_rain_rate, pm_rain_rate))
-
-print("미세먼지 {}".format(lv2))
-print("초미세먼지 {}".format(lv3))
-print("오존지수 {}".format(lv1))
-print("="*50)s
-"""
-
+    # scrape_weather() #오늘의 날씨 정보 가져오기
+    scrape_headline_news()
