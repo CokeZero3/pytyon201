0428f1128a63575edda103840dc8a22319e39ce3
wonho.jeong <jeongwh0310@gmail.com>
2021-01-03 00:54:20 +0900

    update 2

--- 01WebScrapping/10project.py
+++ 01WebScrapping/10project.py
@@ -14,7 +14,7 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
-
+import re
 
 #나도코딩 방법
 def create_soup(url):
@@ -89,13 +89,22 @@ def scrape_it_news():
     print()
     
 def scrape_english():
-
+    print("[오늘의 영어 회화]")
+    url = "https://www.hackers.co.kr/?c=s_eng/eng_contents/I_others_english&keywd=haceng_submain_lnb_eng_I_others_english&logger_kw=haceng_submain_lnb_eng_I_others_english"
+    soup = create_soup(url)
+    sentences = soup.find_all("div", attrs={"id":re.compile("^conv_kor_t")}) #re.compile("") 정규식 모듈로 "" 시작하는 무언가 찾는 코드 
+    print("(영어 지문)")
+    for sentence in sentences[len(sentences)//2]: # 8문장이 있다고 가정할 때, index 기준 4~7까지 잘라서 가져옴
+        print(sentence.get_text().strip())
         
+    print("한글 지문")
+    for sentence in sentences[:len(sentences)//2]: # 8문장이 있다고 가정할 때, index 기준 0~3까지 잘라서 가져옴
+        print(sentence.get_text().strip())
     
 
 if __name__ == "__main__":
-    # scrape_weather() #오늘의 날씨 정보 가져오기
-    # scrape_headline_news() #헤드라인 뉴스
-    # scrape_it_news() #IT 뉴스 정보 가져오기
-    srape_english()
+    scrape_weather() #오늘의 날씨 정보 가져오기
+    scrape_headline_news() #헤드라인 뉴스
+    scrape_it_news() #IT 뉴스 정보 가져오기
+    scrape_english()
     
